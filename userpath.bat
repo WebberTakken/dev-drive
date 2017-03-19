fe9a19174224aa01071529126423cfe6da668880
webber <webber.nl@gmail.com>
2017-03-19 22:43:49 +0100

    Added PATH Reload functionality

--- userpath.bat
+++ userpath.bat
@@ -8,15 +8,21 @@
 ::
 
 :InputSanitation - Check If 2 arguments are set
-If Not "%~1" == "" If Not "%~2" == "" (
-  Echo. & Echo %~0: Adding to PATH Env Variable For this user
+If "%~1" == "" (
+  Goto Usage
+)
+Set CurrentPath=%PATH%
+Set ErrorLevel=
+If Not "%~2" == "" (
   Call :LabelExists "%~1" && Call :%~1 %~2 || Goto Usage
-) Else Goto Usage
+) Else (
+  Call :LabelExists "%~1" && Call :%~1 || Goto Usage
+)
 Goto Exit
 
 :Usage - Show user how to run this programm
 Echo Usage: userpath.bat [Command] [Value]
-Echo Commands: [Add]
+Echo Commands: [Add^|Reload]
 Goto Exit
 
 :LabelExists - Check if function is implemented
@@ -28,8 +34,8 @@ For /f "Delims=" %%t In (
 Goto Exit
 
 :Add - Add a variable to the PATH variable of the current user
+Echo. & Echo Adding to PATH Environment Variable for this user
 Set NewValue=%~1
-Set CurrentPath=%PATH%
 Goto CreateNew
 
 :CreateNew - new PATH variable for user and add data to its value
@@ -38,27 +44,39 @@ If %ErrorLevel% geq 1 (
   Set ErrorLevel=
   Reg Add HKCU\Environment\ /v PATH /t REG_EXPAND_SZ /d %NewValue%; >Nul
   Echo New PATH variable created and added %NewValue%
-  Goto RefreshCurrentSession
+  Goto Reload
 )
 Goto AddToExisting
 
 :AddToExisting - If PATH already exists, add new data to its value
 For /F "tokens=2*" %%A In ('Reg Query HKCU\Environment\ /v PATH') Do Echo.%%B | FindStr /C:"%NewValue%" >Nul && (
    Echo %NewValue% already exists in %%B
-   Echo Done. & Echo.
-   Goto Exit
+   Goto Done
 ) || (
    Echo Adding %NewValue% to %%B
    Reg Delete HKCU\Environment\ /v PATH /f>Nul
    Reg Add HKCU\Environment\ /v PATH /t REG_EXPAND_SZ /d %NewValue%;%%B >Nul
 )
-Goto RefreshCurrentSession
+Goto Reload
 
-:RefreshCurrentSession - Load machine and current user PATH variable for current session
-For /F "tokens=2*" %%A In ('Reg Query HKCU\Environment\ /v PATH') Do Set PATH=%NewValue%;%CurrentPath% >Nul
+:Reload - Load machine and current user PATH variable for current session
+Echo Reloading PATH Environment Variable
+Set ErrorLevel=
+Reg Query HKCU\Environment\ /v PATH >Nul 2>Nul
+If %ErrorLevel% geq 1 (
+  Set ErrorLevel=0
+  Echo Nothing to update from user PATH variable.
+  Goto Done
+)
+If Not "%NewValue%" == "" (
+  Set NewValue=%NewValue%;
+)
+For /F "tokens=2*" %%A In ('Reg Query HKCU\Environment\ /v PATH') Do Set PATH=%NewValue%%CurrentPath% >Nul 2>Nul || Nul
+Goto Done
+
+:Done
 Echo Done. & Echo.
 Goto Exit
 
 :Exit - Exit Session
-
 Exit /b %ErrorLevel%
