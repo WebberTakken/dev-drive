38c82ab6fbc8c289accf29cf00a093326838fb39
Webber <d.webbink@dation.nl>
2017-05-29 20:21:16 +0200

    Added choco, more reliable installation of PHP

--- install.bat
+++ install.bat
@@ -1,39 +1,67 @@
 @Echo Off
 
+::Configuration
+Set InstallDir=%SystemRoot:~0,3%dev\
+Set ChocolateyToolsLocation = %InstallDir%
+Set ChocolateyInstall=%InstallDir%chocolatey\
+
+::SetPermanently
+Setx InstallDir %InstallDir%
+Setx ChocolateyToolsLocation %InstallDir%
+
 ::Refresh PATH Environment Variable
 Call Userpath Refresh > Nul
 
+::Create folder for all tools' installations
+Mkdir %InstallDir% > Nul 2> Nul
+
+::Chocolatey
+:InstallChocolatey
+@powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+ "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+:PermanentlySetInstallDir
+Setx ChocolateyInstall %ChocolateyInstall%
+:AddBinaryToUserPath
+Call Userpath Add %ChocolateyInstall%bin
+:UpdateChocolatey
+choco upgrade chocolatey -yes
+
 ::PHP
-:DownloadPhp
-Call Download http://windows.php.net/downloads/releases/php-7.1.3-nts-Win32-VC14-x86.zip php.zip
-:UnzipPhp
-CScript Unzip.vbs php.zip php
-:UseProductionIni
-Copy /y "%~dp0\php\php.ini-production" "%~dp0\php\php.ini" > Nul
-:EnablePhpSsl
-IniFile.exe %~dp0/php/php.ini [PHP] extension=./ext/php_openssl.dll
-:MovePhpToSystem
-XCopy /e/i/y/h "%~dp0php" "%SystemRoot:~0,3%php" > Nul
-:AddPathForPhpBinary
-Call Userpath Add %SystemRoot:~0,3%php
-:TestPhpInstall
-php -v
+:InstallPhp
+choco install php --yes --params "/InstallDir:%InstallDir%php"
+:UpdatePhp
+choco upgrade php --yes --params "/InstallDir:%InstallDir%php"
+:EnableOpenSsl
+IniFile.exe %InstallDir%php/php.ini [PHP] extension=./ext/php_openssl.dll
+:SetPathIfNoShims
+Call Userpath Add %InstallDir%php
 
 ::Composer
 :InstallComposer
 php -r "copy('https://getcomposer.org/installer', 'composer/composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer/composer-setup.php') !== '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer corrupt'; unlink('composer/composer-setup.php'); }"
+php -r "if (hash_file('SHA384', 'composer/composer-setup.php')!=='669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer corrupt'; unlink('composer/composer-setup.php'); }"
 php composer/composer-setup.php --install-dir=%~dp0/composer --quiet
 php -r "unlink('composer/composer-setup.php');"
 :MoveComposerToSystem
-XCopy /e/i/y/h "%~dp0composer" "%SystemRoot:~0,3%composer" > Nul
+XCopy /e/i/y/h "%~dp0composer" "%InstallDir%composer" > Nul
 :AddPathForComposerFiles
-Call Userpath Add %SystemRoot:~0,3%composer
+Call Userpath Add %InstallDir%composer
 
 ::Symfony
 :InstallSymfony
 MkDir symfony & php -r "readfile('https://symfony.com/installer');" > symfony/symfony
 :MoveSymfonyToSystem
-XCopy /e/i/y/h "%~dp0symfony" "%SystemRoot:~0,3%symfony" > Nul
+XCopy /e/i/y/h "%~dp0symfony" "%InstallDir%symfony" > Nul
 :AddPathForSymfonyFile
-Call Userpath Add %SystemRoot:~0,3%symfony
+Call Userpath Add %InstallDir%symfony
+
+::VerifyInstallations
+:VerifyPhpInstall
+php -v
+:VerifyComposerInstall
+Call composer -V
+:VerifySymfonyInstall
+Call symfony -V
+
+
+
